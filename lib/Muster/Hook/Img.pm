6d72669382009de0ee4aa0bd9fcf1c709000e0f9
Kathryn Andersen <perlkat@katspace.org>
2019-03-17 00:26:18 +1100

    trying to fix Img bug

--- lib/Muster/Hook/Img.pm
+++ lib/Muster/Hook/Img.pm
@@ -197,7 +197,8 @@ sub process {
     else
     {
         my $im = Image::Magick->new();
-        my $r = $im->Read("$format:". $img_info->{filename});
+        ##my $r = $im->Read("$format:". $img_info->{filename});
+        my $r = $im->Read(filename=>$img_info->{filename});
         croak sprintf(("failed to read %s: %s"), $imgpage, $r) if $r;
 
         if (! defined $im->Get("width") || ! defined $im->Get("height"))
